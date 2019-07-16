c7fc1a96a6d7d7a6414532186b8b1357484b021d
galexus <oluwanifemijesuseun@gmail.com>
2019-07-16 12:19:12 +0100

    fix script.js bug

---
+++ script.js
@@ -0,0 +1,10 @@
+// js to add two numbers using prompt function
+
+let firstValue = prompt ("Input Value");
+
+let secondValue  = prompt("Input Second Value");
+
+
+let sum = Number(firstValue) + Number(secondValue);
+
+alert(sum);
